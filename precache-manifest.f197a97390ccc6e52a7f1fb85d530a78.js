25fb6415f3d13308c2a194ab7310ce6715ed4f32
superfoo <mibar1304@gmail.com>
2020-10-04 17:18:47 -0300

    Updates

---
+++ precache-manifest.f197a97390ccc6e52a7f1fb85d530a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bae99726864482dda30aefea22effea",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "51ad1ec345698e1c1fe9",
+    "url": "/curriculum/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "095c039d018d2581864a",
+    "url": "/curriculum/static/js/2.16a53062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curriculum/static/js/2.16a53062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ad1ec345698e1c1fe9",
+    "url": "/curriculum/static/js/main.40a64fd8.chunk.js"
+  },
+  {
+    "revision": "9f5f867cb4b08ff6f999",
+    "url": "/curriculum/static/js/runtime-main.2e812da4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/curriculum/static/media/logo.5d5d9eef.svg"
+  }
+]);
