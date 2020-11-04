c50c50184c9f4de9b9e5763620bbe4c1081855cf
superfoo <mibar1304@gmail.com>
2020-11-04 20:21:05 -0300

    terminada las animaciones de stats containers

--- src/components/content/stats/StatsNav.js
+++ src/components/content/stats/StatsNav.js
@@ -4,17 +4,32 @@ import { SiCodesandbox} from 'react-icons/si'
 import { RiCodeSSlashLine, RiToolsFill } from 'react-icons/ri'
 import {BiPlusCircle} from 'react-icons/bi'
 
+const meta = [
+    {
+        id : "coding",
+        icon: RiCodeSSlashLine,
+        text: "Coding"
+    },{
+        id : "frameworks",
+        icon: SiCodesandbox,
+        text: "Libraries & Frameworks"
+    },{
+        id : "devtools",
+        icon: RiToolsFill,
+        text: "Devtools"
+    },{
+        id : "more",
+        icon: BiPlusCircle,
+        text: "More"
+    }
+]
+
 const StatsNav = ({selectedCategory, setSelectedCategory}) => {
 
     const navigationIndicatorRef = useRef()
     const [ref, setRef] = useState(null)
     
-    useEffect(() => {
-        console.log(ref?.offsetLeft)
-    }, [ref])
-    
     const setCategoryRef = useCallback(element => {
-        // console.log(element?.getAttribute('name'))
         if (element?.getAttribute('name') === selectedCategory){
             setRef(element);
             
@@ -26,38 +41,34 @@ const StatsNav = ({selectedCategory, setSelectedCategory}) => {
         setSelectedCategory(node.getAttribute('name'))
     }
 
+
 	return (
 		<nav>
 			<ul className={Styles.container}>
-				<li className={Styles.categoryContainer} onClick={navigate} ref={setCategoryRef} name="coding" >
-                    <a href="#coding" className={Styles.category}>
-                        <RiCodeSSlashLine className={Styles.categoryIcon}/>
-                        <span className={Styles.categoryText}>Coding</span>
-                    </a>
-                </li>
-				<li className={Styles.categoryContainer} onClick={navigate} ref={setCategoryRef} name="frameworks">
-                    <a href="#frameworks" className={Styles.category}>
-                        <SiCodesandbox className={Styles.categoryIcon}/>
-                        <span className={Styles.categoryText}>Libraries &#38; Frameworks</span>
-                    </a>
-                </li>
-				<li className={Styles.categoryContainer} onClick={navigate} ref={setCategoryRef} name="devtools">
-                    <a href="#devtools" className={Styles.category}>
-                        <RiToolsFill className={Styles.categoryIcon}/>
-                        <span className={Styles.categoryText}>Devtools</span>
-                    </a>
-                </li>
-				<li className={Styles.categoryContainer} onClick={navigate} ref={setCategoryRef} name="more">
-                    <a href="#more" className={Styles.category}>
-                        <BiPlusCircle className={Styles.categoryIcon}/>
-                        <span className={Styles.categoryText}>More</span>
-                    </a>
-                </li>
+                {
+                    meta.map(li => {
+                        let Icon = li.icon;
+                        return (
+                            <li 
+                                key={li.id} 
+                                className={Styles.categoryContainer} 
+                                onClick={navigate} 
+                                ref={setCategoryRef} 
+                                name={li.id} 
+                            >
+                                <a href={`#${li.id}`} className={`${Styles.category} ${selectedCategory === li.id ? Styles.activeNavlink : ''}`}>
+                                    <Icon className={Styles.categoryIcon}/>
+                                    <span className={Styles.categoryText}>{li.text}</span>
+                                </a>
+                            </li>
+                        )
+                    })
+                }
                 <span 
                     ref={navigationIndicatorRef} 
                     style={{
                         width:`${ref ? ref.clientWidth : 0}px`,
-                        left: `${ref ? ref.offsetLeft : 0}px`
+                        left: `${ref ? ref.offsetLeft: 0}px`
                     }}
                     className={Styles.NavigationIndicator}
                 />
